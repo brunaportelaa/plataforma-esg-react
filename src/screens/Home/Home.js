5fafb449006dbfacb03029347144657e30497189
brunaportelaa <brunaporteladj@gmail.com>
2023-04-13 00:05:54 -0300

    Criado o menu de categorias

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,6 +1,7 @@
 import { BodyText } from '../../common-components/BodyText/BodyText'
 import { HeadingOne } from '../../common-components/HeadingOne/HeadingOne'
 import { HeadingTwo } from '../../common-components/HeadingTwo/HeadingTwo'
+import { CategoriaItem } from '../../components/CategoriaItem/CategoriaItem'
 
 export function Home() {
     return (
@@ -9,6 +10,7 @@ export function Home() {
     <HeadingTwo>Dashboard</HeadingTwo>
     <BodyText>Uma ótima tarefa para aqueles que costumam ser os últimos a sair:
 mande uma foto do ar desligado toda vez que for embora.</BodyText>
+    <CategoriaItem />
     </div>
     )
 }
